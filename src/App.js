16182e41eea53ae3337a0de7883c4199fcd841a8
TRETYAKweb <tretyakbro@gmail.com>
2023-01-05 14:29:29 +0300

    fix: react-hooks-useRef: done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react';
 
-import { List } from './List';
+// import { List } from './List';
 
 function App() {
-  const [visibleLest, setVisibleList] = React.useState(true);
+  const [num, setNum] = React.useState([1, 2, 3, 4, 5]);
 
-  const toogleVisibleList = () => {
-    setVisibleList((visible) => !visible);
+  const ulRef = React.useRef();
+  const timerRef = React.useRef();
+
+  const addNum = () => {
+    setNum((prev) => [...prev, prev[prev.length - 1] + 1]);
+    console.log(timerRef.current);
   };
+
+  const start = () => {
+    timerRef.current = setInterval(addNum, 1000);
+  };
+
+  const stop = () => {
+    clearInterval(timerRef.current);
+  };
+
+  const handleScroll = (e) => {
+    console.log(`scroll`);
+  };
+
+  const removeScrol = () => {
+    ulRef.current.removeEventListener('scroll', handleScroll);
+  };
+
+  React.useEffect(() => {
+    ulRef.current.addEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <>
-      {visibleLest && <List />}
-      <button onClick={toogleVisibleList}> Показать / скріть список </button>
+      <ul ref={ulRef}>
+        {num.map((it, index) => (
+          <li key={index}>{it}</li>
+        ))}
+      </ul>
+      <button onClick={addNum}>Добавить число</button>
+      <button onClick={removeScrol}>Не следить</button>
+      <button onClick={start}>Старт</button>
+      <button onClick={stop}>Стоп</button>
     </>
   );
 }
